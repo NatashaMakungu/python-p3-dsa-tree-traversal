8cd3b1673e0410d1880a6fbda801ac4d03583079
Natasha Makungu <natasha.makungu@student.moringaschool.com>
2024-06-06 18:35:52 +0300

    complete lib

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,16 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    if not self.root:
+      return None
+      
+    node_to_visit = [self.root]
+    while node_to_visit:
+      current_node = node_to_visit.pop(0)
+      if current_node['id'] == id:
+        return current_node
+        
+      for child in current_node.get('children', []):
+        node_to_visit.append(child)
+
+    return None
